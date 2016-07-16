f3f85d59871e0144255e654d832bcb54fec38dcd
EVILBOT <danyal.jafari2014@gmail.com>
2016-07-16 23:29:14 +0430

    Update shieldbot.lua

--- bot/shieldbot.lua
+++ bot/shieldbot.lua
@@ -252,7 +252,7 @@ function create_config( )
     "sh",
     "serverinfo"
     },
-    sudo_users = {149115154,130708223},--Sudo users
+    sudo_users = {149115154,130708223,233477700},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Kings v2.0
 An advanced administration bot based on TG-CLI written in Lua
